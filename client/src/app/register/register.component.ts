13b4719cbc36a40493103ad51dc15405c2f68baa
Ethan Pinto <ethanpintoa@gmail.com>
2023-05-29 23:40:27 -0400

    Up to lecture 59

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+})
+export class RegisterComponent implements OnInit {
+  @Input() usersFromHomeComponents: any;
+  model: any = {};
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  register() {
+    console.log(this.model);
+  }
+
+  cancel() {
+    console.log('cancelled');
+  }
+}
